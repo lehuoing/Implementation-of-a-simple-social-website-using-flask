33613903ff7648c6e58d0dbadd75f77d33e56a2b
Zhihao Ye <zhihao.ye1@student.unsw.edu.au>
2017-10-14 23:44:32 +1100

    can show images

--- UNSWtalk.py
+++ UNSWtalk.py
@@ -7,7 +7,8 @@
 import os
 from flask import Flask, render_template, session
 
-students_dir = "dataset-medium";
+# students_dir = "dataset-medium";
+students_dir = "dataset-small";
 
 app = Flask(__name__)
 
@@ -21,10 +22,18 @@ def start():
     students = sorted(os.listdir(students_dir))
     student_to_show = students[n % len(students)]
     details_filename = os.path.join(students_dir, student_to_show, "student.txt")
+    image_filename = os.path.join(students_dir, student_to_show, "img.jpg")
     with open(details_filename) as f:
         details = f.read()
+    f.close()
+    f = open(image_filename,'rb')
+    image_data = f.read()
+    f.close()
+    f = open("./static/"+student_to_show+".jpg",'wb')
+    f.writelines(image_data)
+    f.close()
     session['n'] = n + 1
-    return render_template('start.html', student_details=details)
+    return render_template('start.html', student_details=details,img_path="./static/"+student_to_show+".jpg")
 
 if __name__ == '__main__':
     app.secret_key = os.urandom(12)
